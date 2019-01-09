9ce33baf707b2105dd6bd417f5697a2d35e04358
Steven Van Iseghem <steven.van.iseghem@outlook.com>
2019-01-09 17:34:14 +0100

    Add project files.

---
+++ ConsoleAppDebuggingLambdaExpressions/LambdaExample.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDebuggingLambdaExpressions
+{
+    class LambdaExample
+    {
+        public string FavThings { get; set; }
+    }
+}
---
+++ ConsoleAppDebuggingLambdaExpressions/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDebuggingLambdaExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<LambdaExample> favThings = new List<LambdaExample>();
+
+            LambdaExample thing1 = new LambdaExample();
+            thing1.FavThings = "Ice-cream";
+            favThings.Add(thing1);
+
+            LambdaExample thing2= new LambdaExample();
+            thing2.FavThings = "Summer Rain";
+            favThings.Add(thing2);
+
+            LambdaExample thing3 = new LambdaExample();
+            thing3.FavThings = "Sunday Morning Snooze";
+            favThings.Add(thing3);
+
+            // Copy Lambda Expression in Debug - Windows - Watch1 "favThings.Where(feature => feature.FavThings.StartsWith("Sum"))"
+            var filteredFavThings = favThings.Where(feature => feature.FavThings.StartsWith("Sum"));
+        }
+    }
+}
